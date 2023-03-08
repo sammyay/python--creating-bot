4c96a748377481e83c8b4c76edcc5acf7561bc7d
sammyay <sammyayo600@gmail.com>
2023-03-09 00:43:10 +0100

    delete python.py

--- python.py
+++
@@ -1,9 +0,0 @@
-x =int(input("Enter the first number: "))
-y =int(input("Enter the second number: "))
-
-sum = x + y
-
-if sum > 100:
-    print("The sum is large")
-if sum <= 100:
-    print("The sum is too small")
